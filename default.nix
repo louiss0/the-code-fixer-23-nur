e9794829d759485cd1694eec12d671c361bf9f2f
Shelton Louis <louisshelton0@gmail.com>
2025-07-09 14:48:26 -0400

    chore: Initial commit1

--- default.nix
+++ default.nix
@@ -6,7 +6,9 @@
 # commands such as:
 #     nix-build -A mypackage
 
-{ pkgs ? import <nixpkgs> { } }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 
 {
   # The `lib`, `modules`, and `overlays` names are special
@@ -15,6 +17,6 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
+  jpd = pkgs.callPackage ./pkgs/javascript-package-delegator { };
   # ...
 }
