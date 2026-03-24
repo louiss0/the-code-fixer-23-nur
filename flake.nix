d04a80014a32380983d8399fa52a1649459a3469
louiss0 <louisshelton0@gmail.com>
2026-03-24 19:33:10 -0400

    fix(repo): remove forced default package

--- flake.nix
+++ flake.nix
@@ -9,12 +9,6 @@
       legacyPackages = forAllSystems (system: import ./default.nix {
         pkgs = import nixpkgs { inherit system; };
       });
-      packages = forAllSystems (system:
-        let
-          packages = nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system};
-        in
-        packages // {
-          default = packages.go-toolkit;
-        });
+      packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
     };
 }
